385d7e3f175aa45f4f016b0df46a7c88fe9e66c6
Yuri Y. Kuznetsov <yyk@kestrel>
2021-12-24 15:45:22 -0500

    Added a Button to FormApp3

--- mobile_junk/FormsApp3/FormsApp3/MainPage.xaml.cs
+++ mobile_junk/FormsApp3/FormsApp3/MainPage.xaml.cs
@@ -14,5 +14,11 @@ namespace FormsApp3
         {
             InitializeComponent();
         }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
     }
 }
