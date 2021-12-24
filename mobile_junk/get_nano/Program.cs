fd323eaf29b6e0ec49bfdd1d5a3e97a59a5fac38
Yuri Y. Kuznetsov <yyk@kestrel>
2021-12-24 15:56:44 -0500

    Added get_nano project

---
+++ mobile_junk/get_nano/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace get_nano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
