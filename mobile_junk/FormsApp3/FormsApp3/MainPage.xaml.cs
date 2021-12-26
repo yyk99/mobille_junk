40e31db02f09df7b2eaced5d5c90df823c0028b6
Yuri Y. Kuznetsov <yyk@kestrel>
2021-12-26 06:02:23 -0500

    Added Version button

--- mobile_junk/FormsApp3/FormsApp3/MainPage.xaml.cs
+++ mobile_junk/FormsApp3/FormsApp3/MainPage.xaml.cs
@@ -31,7 +31,8 @@ namespace FormsApp3
                 nano = new Nano();
                 tsk = nano.GetRecord();
                 butt.Text = "Started...";
-            } else
+            }
+            else
             {
                 if (tsk.IsCompleted)
                 {
@@ -40,7 +41,8 @@ namespace FormsApp3
                     tsk.Dispose();
                     nano = null;
                     tsk = null;
-                } else
+                }
+                else
                 {
                     butt.Text = "In progress...";
                 }
@@ -57,6 +59,18 @@ namespace FormsApp3
             var nano = new Nano();
             var r = await nano.GetRecord();
             butt.Text = $"{count}: HUM: {r.h / 100.00}% TEMP: {r.c / 100.00}C {r.f / 100.00}F";
+        }
+
+        // Handle_Clicked_Version
+        async void Handle_Clicked_Version(object sender, System.EventArgs e)
+        {
+            var butt = (Button)sender;
+
+            butt.Text = "0.0.1";
+
+            await Task.Delay(1000);
+
+            butt.Text = "Version";
         }
     }
 }
